8af5cc018a53da9ef0fb19f4962abd1367068a60
Robert D <152869096+ripple-caster@users.noreply.github.com>
2023-12-05 13:56:32 +0600

    Create core.py

---
+++ core.py
@@ -0,0 +1,15 @@
+# core.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise MathError("Cannot divide by zero")
+    return a / b
